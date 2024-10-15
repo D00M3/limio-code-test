5c584c6d63bf44ac4e97a3c4b82e2d6eed05f4c8
D00M3 <zak1231976@outlook.com>
2024-10-15 14:38:24 +0100

    fixed <div> cannot appear as a descendant of <p>.

--- src/components/limio-footer/components/ContactFields/index.js
+++ src/components/limio-footer/components/ContactFields/index.js
@@ -6,17 +6,19 @@ const ContactFields = ({ contactFields }: ContactFieldsProps) => (
   <>
     {contactFields.map((contactField, index) =>
       contactField.url ? (
+        <div className="ContactLabel">{contactField.label}
         <p key={"contactField" + index} className={"ContactField"}>
-          <div className="ContactLabel">{contactField.label}</div>
           <a className="FooterLink" href={contactField.url} target="_top">
             {contactField.value}
           </a>
         </p>
+        </div>
       ) : (
+        <div className="ContactLabel">{contactField.label}
         <p className={"ContactField"} key={"contactField" + index}>
-          <div className="ContactLabel">{contactField.label}</div>
           {contactField.value}
         </p>
+        </div>
       )
     )}
   </>
